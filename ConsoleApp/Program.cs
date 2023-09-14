dcb2a4857fb8c345346c22f0b6e052dfd01b70bb
julianclaramunt <142940710+julianclaramunt@users.noreply.github.com>
2023-09-14 01:38:57 -0300

    Actualizacion de mensajes

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("UnLAM - PA2 - TP0");
+Console.WriteLine("Proyecto generado desde Visual Studio 2022");
